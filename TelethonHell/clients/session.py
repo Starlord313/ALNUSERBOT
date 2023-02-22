b8da5431ef2f7026b582462fd74c215d792ca5a4
Starlord313 <starlordgaming57@gmail.com>
2023-02-22 11:20:06 +0530

    Update session.py

--- TelethonHell/clients/session.py
+++ TelethonHell/clients/session.py
@@ -6,6 +6,13 @@ from telethon.sessions import StringSession
 
 from HellConfig import Config
 
+def validate_session(session):
+    if "==hell" and "bot==" in session.lower():
+        new_session = session[6:-5]
+        return str(new_session)
+    else:
+        print(f"ALNUB SESSION - Wrong session string!")
+        sys.exit()
 if Config.ALNUB_SESSION:
 
     session = StringSession(validate_session(Config.ALNUB_SESSION))
